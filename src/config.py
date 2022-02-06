eb004e39ce55f3408968db813ece074020d614d4
Tianqi Tong <tianqi.tong@svca.cc>
2022-02-05 21:58:10 -0800

    add platform dependent configs

--- src/config.py
+++ src/config.py
@@ -1,1 +1,8 @@
-LOCAL_SQLITE_DB_FILE = "/Users/tianqi_tong/tmp/rpi_meow/rpi_meow.db"
+import platform
+
+if platform.system() == 'Darwin':
+    LOCAL_SQLITE_DB_FILE = "/Users/tianqi_tong/tmp/rpi_meow/rpi_meow.db"
+    LAST_TIME_SAW_CAT_IMG = "/Users/tianqi_tong/tmp/rpi_meow/last_time_saw_cat.jpg"
+elif platform.system() == 'Linux':
+    LOCAL_SQLITE_DB_FILE = "/home/pi/dev/rpi_meow/rpi_meow.db"
+    LAST_TIME_SAW_CAT_IMG = "/home/pi/dev/rpi_meow/last_time_saw_cat.jpg"
